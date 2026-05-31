952c9c06437828d510966808da7596749adedbb1
jaybamania <90679000+jaykumarbamania@users.noreply.github.com>
2026-05-31 11:47:32 +0530

    Created and Registered LLM with Dummy tool

--- main.py
+++ main.py
@@ -8,9 +8,33 @@ app = FastAPI()
 
 llm = ChatOpenAI(model="gpt-4o-mini")
 
+
+#Create tool
+from langchain.tools import tool
+
+@tool
+def get_company_name():
+    """Returns company name"""
+
+    return "Jay OMS Pvt Ltd"
+
+#Register tool
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
+tools = [get_company_name]
+
+agent = initialize_agent(
+    tools=tools,
+    llm=llm,
+    agent=AgentType.OPENAI_FUNCTIONS,
+    verbose=True
+)
+
+
 @app.get("/ask")
 def ask(question: str):
-    response = llm.invoke(question)
+    # response = llm.invoke(question)
+    response = agent.run(question)
     return {
         "answer": response.content
     }
