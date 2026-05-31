2d56caf11f553789f33b69d829c016a211852aa4
jaybamania <90679000+jaykumarbamania@users.noreply.github.com>
2026-06-01 00:50:55 +0530

    Seperated Configs and added Debug logs

--- app/tools/order_tool.py
+++ app/tools/order_tool.py
@@ -1,16 +1,24 @@
 import requests
 
 from langchain_core.tools import tool
+from app.config.app_config import CLIENT_SERVICE_BASE_URL
+
 
 
 @tool
 def get_order_status(order_id: str):
     """
-    Fetch order information using order id
+    Fetch order details.
+
+    Args:
+        order_id: Unique order identifier
+
+    Returns:
+        orderId, customerName, status, amount
     """
 
     response = requests.get(
-        f"http://localhost:8080/orders/{order_id}"
+        f"{CLIENT_SERVICE_BASE_URL}/orders/{order_id}"
     )
 
     return response.json()
